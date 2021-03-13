3d4e064489e75d5b8f747498f819f0e3274bdfb9
Xuehong <xuehong.liu.pdx@gmail.com>
2021-03-13 13:07:55 -0800

    add balanced_sum.js

--- circular_queue.js
+++ circular_queue.js
@@ -1,7 +1,8 @@
 class CircularQueue {
-    /* writes to the end of a collection then begins overwriting 
-    itself at the beginning of the collection */
-    constructor(size) {
+  /* writes to the end of a collection then begins 
+  overwriting itself at the beginning of the 
+  collection */
+  constructor(size) {
       this.queue = [];
       this.read = 0;
       this.write = 0;
@@ -18,22 +19,29 @@ class CircularQueue {
     }
   
     enqueue(item) {
-      nulls=this.queue.filter(x=> x!==null)
-      if (nulls.length===0) {
-        this.queue[0]=item
-        return item
-      } else {
-          for (i=0; i<this.queue.length; i++){
-            if (this.queue[i]===null){
-              this.queue[i]=item
-              return item
-            }
-          }
-        }
+      if (this.queue[this.write] === null) {
+        this.queue[this.write++] = item;
+  
+        if (this.write > this.max) this.write = 0;
+        return item;
       }
-    
+      return null;
+    }
+  
     dequeue() {
-      let before = this.queue.filter(x=> x < item)
-      let after = this.queue.filter(x=> x > item)
+      if (this.queue[this.read] != null) {
+        let item = this.queue[this.read];
+        this.queue[this.read++] = null;
+        if (this.read > this.max) this.read = 0;
+        return item;
+      }
+      return null;
     }
   }
+
+b = new CircularQueue(3)
+console.log('enqueue: ', b.enqueue(1))
+console.log('enqueue: ', b.enqueue(2))
+console.log('enqueue: ', b.enqueue(3))
+console.log('dequeue: ', b.dequeue())
+console.log('queue: ', b.queue)
--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-console.log(Math.sqrt(20))
+/* Check whether there exists an element in the array such that 
+sum of elements on its left is equal to the sum of elements on its 
+right. */
+
+function balancedSums(arr) {
+    if (arr.length===1){
+        return 'YES'
+    } else if (arr.length===2) {
+        return 'NO'
+    } else {
+        for (i=1; i<arr.length-1; i++) {
+            left = arr.slice(0,i)
+            right = arr.slice(i+1, arr.length)
+            leftSum = left.reduce((sum, x) => sum +=x)
+            rightSum = right.reduce((sum, x) => sum +=x)
+            if ( leftSum === rightSum){
+                return 'YES';
+            }
+        }
+        return 'NO';
+    }
+}
+
+console.log(balancedSums([1,1,4,1,1]))
