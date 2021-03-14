025590b055513bd1346318b050976315374d2399
Xuehong <xuehong.liu.pdx@gmail.com>
2021-03-14 11:55:21 -0700

    use nested setTimeout in  dancing_balls.js

--- dancing_balls.js
+++ dancing_balls.js
@@ -18,23 +18,19 @@ class Bubble {
   }
 }
 
-function doSetInterval(i) {
-    function run() {
-        const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        
-        let rx1 = 50 + 50*Math.random(-i, i);
-        let ry1 = 50 + 80*Math.cos(Math.random(-i, i));
-        let bubble1 = new Bubble(rx1, ry1, 'red', 10);
-        bubble1.draw();
+let timerId = setTimeout(function run() {
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
     
-        let rx2 = 50+ 80*Math.sin(Math.random(-i, i));
-        let ry2 = 50+ 50*Math.cos(Math.random(-i, i));
-        let bubble2 = new Bubble(rx2, ry2, 'green', 10);
-        bubble2.draw();
-    }
+    let rx1 = 50 + 50*Math.random(-1, 1);
+    let ry1 = 50 + 80*Math.cos(Math.random(-1, 1));
+    let bubble1 = new Bubble(rx1, ry1, 'red', 10);
+    bubble1.draw();
 
-    setInterval(run, 300);
-  }
-  
-  doSetInterval(5);
+    let rx2 = 50+ 80*Math.sin(Math.random(-1, 1));
+    let ry2 = 50+ 50*Math.cos(Math.random(-1, 1));
+    let bubble2 = new Bubble(rx2, ry2, 'green', 10);
+    bubble2.draw();
+
+    timerId = setTimeout(run, 300); 
+  }, 10);
